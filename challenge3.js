c3dcafb45c0fb34ce67b9b56b0d14b65fea56943
praiseGit34 <praiseasiimire38@gmail.com>
2024-08-10 06:28:52 +0300

    attempt challenge 3

---
+++ challenge3.js
@@ -0,0 +1,20 @@
+// Write a function named twoSum which takes two parameters: nums
+// and target. Given an array of integer nums and an integer target, return
+// indices of the two numbers such that they add up to the target. You
+// may assume that each input would have exactly one solution, and you
+// may not use the same element twice.
+// You can return the answer in any order.
+// nums=[2,7,11,15];
+// target=9;
+function twoSum(nums,target){
+    for(let i=0;i<nums.length;i++){
+        for(let j=0;j<nums.length;j++){
+            if(nums[i]+nums[j]===target){
+                return [i,j];//return indices
+            }
+        }
+    }
+    return [];
+}
+const answer=twoSum([2,7,11,15] ,26);//function call
+console.log(answer);
